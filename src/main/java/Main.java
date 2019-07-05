5ecc527b0bf04ebe469ada54f747ed314f6d2ad9
Sam <samgauck@icloud.com>
2019-07-05 14:36:50 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * A Javadoc template. TODO: Update Main Javadoc.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+    }
+}
